e8da93b8d1c858a35ed921594f361544208fa1bd
Thomas <olleck@gmail.com>
2022-06-23 19:01:55 +0200

    __create_fds_row_table and initialize tables in __init__ method of db client

--- src/infrastructure/DatabaseClient.py
+++ src/infrastructure/DatabaseClient.py
@@ -1,16 +1,22 @@
-#!/usr/bin/python
 # define connection and cursor
 import sqlite3
 
 
 class DatabaseClient:
     def __init__(self, database_path):
+        # initialize database connection & store open connection as class member variable
         self.connection = sqlite3.connect(database_path)
 
-    def create_measurements_table(self):
+        # create material measurement and fds row table => IF NOT EXISTS
+        # we do this in the __init__ method because every database client needs those tables
+        # in order to work
+        self.__create_material_measurement_table()
+        self.__create_fds_row_table()
+
+    def __create_material_measurement_table(self):
         cursor = self.connection.cursor()
-        create_measurements_table = """
-            CREATE TABLE IF NOT EXISTS measurement(
+        create_material_measurement_table = """
+            CREATE TABLE IF NOT EXISTS MaterialMeasurement(
                 ID INTEGER PRIMARY KEY,
                 MaterialName TEXT,
                 Temperature  REAL,
@@ -21,7 +27,22 @@ class DatabaseClient:
                 MeasurementStarted DATETIME
             )
         """
-        cursor.execute(create_measurements_table)
+        cursor.execute(create_material_measurement_table)
+        self.connection.commit()
+
+    # TODO: create column names for FDS Table values
+    def __create_fds_row_table(self):
+        cursor = self.connection.cursor()
+        create_fds_row_table = """
+            CREATE TABLE IF NOT EXISTS FDSRow(
+                ID INTEGER PRIMARY KEY,
+                MeasurementID INTEGER,
+                Frequency REAL,
+
+                FOREIGN KEY (MeasurementID) REFERENCES Measurement(ID)
+            )
+        """
+        cursor.execute(create_fds_row_table)
         self.connection.commit()
 
     def close_database_connection(self):
